2de6f1aaa2779b650bdbd90215d97f2a443ccede
Jason Weitz <jasonweitz@visitor-10-56-155-28.wifi.berkeley.edu>
2025-06-25 11:24:37 -0700

    fixed coordinates

--- plot_cities.py
+++ plot_cities.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
-cities = [["Chicago", 41.8781, 87.6298],
-          ["Boston", 42.3555, 71.0565],
-          ["Madison", 43.0722, 89.4008],
-          ["San Diego", 32.7157, 117.1611]]
+cities = [["Chicago", 41.8781, -87.6298],
+          ["Boston", 42.3555, -71.0565],
+          ["Madison", 43.0722, -89.4008],
+          ["San Diego", 32.7157, -117.1611]]
 scale = 5
 
 map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
