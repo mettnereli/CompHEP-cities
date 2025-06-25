c58d1e0575e79a57a5e4c345f0f71203f6ea2952
Jenna Roderick <jroderick2@wisc.edu>
2025-06-25 13:44:44 -0500

    Colored the land green and ocean blue

--- plot_cities.py
+++ plot_cities.py
@@ -7,11 +7,16 @@ scale = 5
 
 map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
         projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
+#Adding color to the map
+map.drawlsmask(land_color='green',ocean_color='lightblue',lakes=True)
+map.drawstates(color='black')
 
-map.drawstates()
 
 # Get the location of each city and plot it
 for (city, latitude, longitude) in cities:
     x, y = map(longitude, latitude)
     map.plot(x, y, marker='o',color='Red')
 plt.show()
+
+plt.savefig('map.png', dpi=300, bbox_inches='tight')
+
